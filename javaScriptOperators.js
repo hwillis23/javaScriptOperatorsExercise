fd8e82c206696ee22450a45b781f971d857c3b99
Hannah Willis <hwillis8817@gmail.com>
2022-04-25 12:20:01 -0500

    completed

---
+++ javaScriptOperators.js
@@ -0,0 +1,48 @@
+let a =20;
+let b = 4;
+let add = a +b;
+let minus = add - b;
+let multiply = minus * a;
+let dividing = a/b;
+ 
+
+console.log(a); //20
+console.log(b); //4
+console.log(add); //24
+console.log(minus); //20
+console.log(multiply); //400
+console.log (dividing); //5
+
+
+//Exercise 2
+let c = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+console.log(c + str); 
+console.log(c + str2);
+console.log(c + isPresent);
+console.log(c + firstName);
+console.log(c + lastName);
+
+//Exercise 3 
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+console.log(d == str3); //true
+console.log(d === str4); //false 
+console.log(!isPresent2); //true
+console.log("eleven" == str3 && d>= str4); //false 
+console.log(!isPresent2 || isPresent2); //true 
+console.log(0 == false); //true 
+console.log(0 === false); //false
+console.log(0 != false); // false
+console.log(0 !== false); //true 
+
